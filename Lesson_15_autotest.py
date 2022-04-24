568b078bbeeff41dd18b64340704db59a1b6ea75
Daniil K <56474558+daniil311@users.noreply.github.com>
2022-04-24 22:59:02 +0500

    Задание: принимаем alert
    В этой задаче вам нужно написать программу, которая будет выполнять следующий сценарий:
    
    1.Открыть страницу http://suninjuly.github.io/alert_accept.html
    2.Нажать на кнопку
    3.Принять confirm
    4.На новой странице решить капчу для роботов, чтобы получить число с ответом

---
+++ Lesson_15_autotest.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+import math
+
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button1 = browser.find_element_by_tag_name("button")
+    button1.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_tag_name("button")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+finally:
+    # успеваем скопировать код за 5 секунд
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
