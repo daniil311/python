5bc847f3e466858ab626a273c33dd54bfcf02da5
Daniil K <56474558+daniil311@users.noreply.github.com>
2022-04-25 21:12:02 +0500

    text_to_be_present_in_element
    Задание: ждем нужный текст на странице
    Попробуем теперь написать программу, которая будет бронировать нам дом для отдыха по строго заданной цене. Более высокая цена нас не устраивает, а по более низкой цене объект успеет забронировать кто-то другой.
    
    В этой задаче вам нужно написать программу, которая будет выполнять следующий сценарий:
    
    1.Открыть страницу http://suninjuly.github.io/explicit_wait2.html
    2.Дождаться, когда цена дома уменьшится до $100 (ожидание нужно установить не меньше 12 секунд)
    3.Нажать на кнопку "Book"
    4.Решить уже известную нам математическую задачу (используйте ранее написанный код) и отправить решение

---
+++ Lesson_18_autotest.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+try:
+    browser = webdriver.Chrome()
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    # говорим Selenium проверять в течение 12 секунд, пока кнопка не станет кликабельной
+    WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"),"$100")
+    )
+    btn = browser.find_element_by_id("book")
+    btn.click()
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", input1)
+    input1.send_keys(y)
+
+    button2 = browser.find_element_by_id("solve")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button2)
+    button2.click()
+
+finally:
+    # успеваем скопировать код за 5 секунд
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
